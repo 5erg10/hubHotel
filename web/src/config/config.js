9ef547cc37683c1d9337dd75b55e123de658894d
5erg1kal <santamaria.fajardo@gmail.com>
2026-03-19 09:36:16 +0100

    change env variable values

--- web/src/config/config.js
+++ web/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     socketUrl: import.meta.env.VITE_SOCKET_URL,
-    dataSourceUrl: import.meta.env.VITE_SOCKET_URL
+    dataSourceUrl: import.meta.env.VITE_SERVICE_URL
 }
 
 export default config;
